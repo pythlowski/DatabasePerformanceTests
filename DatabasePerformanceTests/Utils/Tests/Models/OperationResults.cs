6f0b1b9543cc6b7d58e8c72adf1213990ce7805a
pythlowski <ikswoltyp@gmail.com>
2025-01-11 16:31:43 +0100

    feat: xd2

---
+++ DatabasePerformanceTests/Utils/Tests/Models/OperationResults.cs
@@ -0,0 +1,58 @@
+using DatabasePerformanceTests.Data.Operations;
+using DatabasePerformanceTests.Utils.Config.Enums;
+
+namespace DatabasePerformanceTests.Utils.Tests.Models;
+
+public class OperationResults
+{
+    public OperationResults(OperationType operationType, int? dataSize)
+    {
+        OperationType = operationType;
+        DataSize = dataSize;
+        Results = new();
+    }
+
+    public OperationType OperationType { get; set; }
+    public int? DataSize { get; set; }
+    public Dictionary<DatabaseSystem, SystemResults> Results { get; set; }
+    public override string ToString() => $"{OperationType} for {DataSize ?? -1} data size";
+    
+    public void RegisterSystemResults(DatabaseSystem system, SystemResults systemResults)
+    {
+        Results.Add(system, systemResults);
+    }
+}
+
+public class SystemResults
+{
+    public SystemResults()
+    {
+        ExecutionTimesMilliseconds = new();
+    }
+
+    public List<int> ExecutionTimesMilliseconds { get; set; }
+    public double Average { get; set; }
+    public double Median { get; set; }
+    public double Minimum { get; set; }
+    public double Maximum { get; set; }
+
+    public void RegisterTime(int timeMilliseconds)
+    {
+        ExecutionTimesMilliseconds.Add(timeMilliseconds);
+    }
+    public void CalculateResultParameters()
+    {
+        Average = ExecutionTimesMilliseconds.Average();
+        Median = GetMedian(ExecutionTimesMilliseconds);
+        Minimum = ExecutionTimesMilliseconds.Min();
+        Maximum = ExecutionTimesMilliseconds.Max();
+    }
+    
+    private static double GetMedian(List<int> values)
+    {
+        return values.OrderBy(item => item)     // from sorted sequence
+            .Skip((values.Count - 1) / 2)  // we skip leading half items
+            .Take(2 - values.Count % 2)    // take one or two middle items 
+            .Average();
+    }
+}
